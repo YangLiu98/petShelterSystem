891bd23a0cd96a83c09f44a8fdac54bf32204509
lenovo <2805107837>
2020-05-12 09:29:22 +0800

    图片上传，未加水印

---
+++ src/main/java/com/example/demo/controller/uploadController.java
@@ -0,0 +1,54 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+
+@Controller
+public class uploadController {
+    //跳转到上传文件的页面
+    @RequestMapping(value = "/upload", method = RequestMethod.GET)
+    public String goUploadImg() {
+        //跳转到 templates 目录下的 uploadimg.html
+        System.out.println("登陆界面");
+        return "upload";
+    }
+
+    //处理文件上传
+    @ResponseBody //返回json数据
+    @RequestMapping(value = "/testUploadimg", method = RequestMethod.POST)
+    public String uploadImg(@RequestParam("file") MultipartFile file, HttpServletRequest request) {
+        String contentType = file.getContentType();
+        String fileName = file.getOriginalFilename();
+        String filePath = "D:/img";
+        if (file.isEmpty()) {
+            return "文件为空！";
+        }
+        try {
+            uploadFile(file.getBytes(), filePath, fileName);
+        } catch (Exception e) {
+            // TODO: handle exception
+        }
+        //返回json
+        return "上传成功";
+    }
+
+    public static void uploadFile(byte[] file, String filePath, String fileName) throws Exception {
+        File targetFile = new File(filePath);
+        if (!targetFile.exists()) {
+            targetFile.mkdirs();
+        }
+        FileOutputStream out = new FileOutputStream(filePath +"/"+ fileName);
+        System.out.println("已存储");
+        out.write(file);
+        out.flush();
+        out.close();
+    }
+}
